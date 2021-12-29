da8103dea0d73a63e222363ceeeede1bc9d7eae7
Carson Rueber <carsonrueber@gmail.com>
2021-12-28 21:07:28 -0800

    Fix bug

--- src/VerbEntry.cpp
+++ src/VerbEntry.cpp
@@ -1,5 +1,6 @@
 #include "VerbEntry.hpp"
 #include "ConjugationEntry.hpp"
+#include <iostream>
 
 const static wxString verbTenses[] = {
 	"Pr\u00E9sent",
@@ -7,18 +8,13 @@ const static wxString verbTenses[] = {
 	"Futur",
 	"Pass\u00E9 Simple",
 	"Conditionnel"
-	"0"
 };
 
 VerbEntry::VerbEntry(wxWindow* parent) : wxPanel(parent) {
 	int x,y;
 	parent->GetClientSize(&x,&y);
 	notebook = new wxNotebook(this, 1, wxPoint(0,0), wxSize(x,y));
-	int q = 0;
-	while (true) {
-		if (verbTenses[q] == "0")
-			break;
+	for (int q = 0; q < sizeof(verbTenses) / sizeof(wxString); q++) {
 		notebook->AddPage(new ConjugationEntry(notebook), verbTenses[q]);
-		q++;
 	}
 }
